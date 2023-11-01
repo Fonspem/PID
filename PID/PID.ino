ccfa2d00be80df9e7a39e32a299207f443c288aa
Fonspem <fonspem@gmail.com>
2023-11-01 20:22:04 -0300

    Update PID.ino
    Introduciendo PD al algoritmo.

--- PID/PID.ino
+++ PID/PID.ino
@@ -5,7 +5,11 @@ const float t_ambiente {28};
 float t_horno {175};
 const float t_max {300};
 float t_deseada {200};
+
+//P [%]
 float porcentaje_banda {10};
+//D [s]
+float constante_derivariva {5};
 
 bool horno {true};
 bool calentador {false};
@@ -18,9 +22,26 @@ const float dt_apagado {3.0};
 int pin_temperatura = A5;
 int pin_calentador = 2;
 
-float salida_proporcional_porcentaje(float t_deseada, float banda, float t_actual)// °C,%,°C -> %
+float salida_proporcional_porcentaje(float t_deseada, float t_actual, float banda)// °C,°C,% -> %
 {
   float x { (t_deseada - t_actual)*100.0 / (t_deseada*banda/100) };
+    
+  if(x > 100){
+    return 100;
+  }
+  if(x < 0){}
+    return 0;
+  }
+  return x;
+}
+
+float proporcional_derivativa_porcentaje(float t_deseada, float t_actual, float banda, float const_d)// °C,°C,%,k -> %
+{
+  float Kp,E,Kd,x;
+
+  Kp = 100.0 / (banda*t_deseada/100.0);
+  Kd = (100.0 * const_d) / (banda*t_deseada/100.0);
+  E = t_deseada - t_actual;
     
   if(x > 100){
     return 100;
@@ -46,6 +67,7 @@ void calentador_on_off(int PWM){
       t_horno += (dt_encendido/(60000/delay_en_ms)) * PWM / 100;
     }  
 }
+
 void perdidas_horno(){
   if(t_horno > t_ambiente){
     t_horno -= dt_apagado/(60000/delay_en_ms);
@@ -74,7 +96,7 @@ void loop()
   lcd_1.setCursor(6, 0);
   lcd_1.print(t_horno);
   
-  float pwm = salida_proporcional_porcentaje(t_deseada,porc_banda,t_horno);
+  float pwm = salida_proporcional_porcentaje(t_deseada, t_horno, porc_banda);
   
   lcd_1.setCursor(6, 1);
   lcd_1.print(pwm);
